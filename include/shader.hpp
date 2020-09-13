ba99b12dbe857712c746b26d205db07ade45ceb0
Hernán Valdés <hfvaldesg@gmail.com>
2020-09-13 17:44:04 +0200

    Make two methods of the Shader class static

---
+++ include/shader.hpp
@@ -0,0 +1,22 @@
+#ifndef SHADER_HPP
+#define SHADER_HPP
+
+extern "C" {
+	// Include the GLAD loader *before* including GLFW!
+	#include "glad/glad.h"
+	
+	// Include the GLFW library (should be the same for all OS):
+	#include <GLFW/glfw3.h>
+}
+
+class Shader {
+   private:
+    int id;
+    static char* read_from_file(const char* path);
+    static GLuint compile(GLenum type, const char* shader_path, const char* shader_tag);
+   public:
+    Shader(const char* path);
+    GLuint compile();
+};
+
+#endif
